fe11c8dd488e153caa52a6f53168c1bd06774e2d
Mahmud <jakirmahi20009>
2024-05-03 00:50:33 +0600

    bug fix

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,22 +1,22 @@
 import React, { lazy, Suspense } from "react";
-// import Aboutus from "../../Components/Aboutus/Aboutus";
-// import Ourmission from "../../Components/OurMission/Ourmission";
-// import Landingpage from "../../Components/Landingpage/Landingpage";
-// import Membership from "../../Components/Membership/Membership";
-// import Ourvalues from "../../Components/Ourvalues/Ourvalues";
-// import Quotes from "../../Components/Quotes/Quotes";
+import Aboutus from "../../Components/Aboutus/Aboutus";
+import Ourmission from "../../Components/OurMission/Ourmission";
+import Landingpage from "../../Components/Landingpage/Landingpage";
+import Membership from "../../Components/Membership/Membership";
+import Ourvalues from "../../Components/Ourvalues/Ourvalues";
+import Quotes from "../../Components/Quotes/Quotes";
 
 import { useEffect } from "react";
 import { setDocumentTitle } from "../../Utils/DocumentTitle";
 import PrayerWidget from "../../Components/Widget/prayerWidget";
-const Landingpage = lazy(() =>
-  import("../../Components/Landingpage/Landingpage")
-);
-const Ourmission = lazy(() => import("../../Components/OurMission/Ourmission"));
-const Aboutus = lazy(() => import("../../Components/Aboutus/Aboutus"));
-const Membership = lazy(() => import("../../Components/Membership/Membership"));
-const Ourvalues = lazy(() => import("../../Components/Ourvalues/Ourvalues"));
-const Quotes = lazy(() => import("../../Components/Quotes/Quotes"));
+// const Landingpage = lazy(() =>
+//   import("../../Components/Landingpage/Landingpage")
+// );
+// const Ourmission = lazy(() => import("../../Components/OurMission/Ourmission"));
+// const Aboutus = lazy(() => import("../../Components/Aboutus/Aboutus"));
+// const Membership = lazy(() => import("../../Components/Membership/Membership"));
+// const Ourvalues = lazy(() => import("../../Components/Ourvalues/Ourvalues"));
+// const Quotes = lazy(() => import("../../Components/Quotes/Quotes"));
 
 const Home = () => {
   const handleLoading = () => {
