1ced33809ab076b3e8644688a7d4d006dd0c2355
Mahmud <jakirmahi20009>
2024-06-28 17:34:20 +0600

    update

---
+++ src/Components/Widget/Prayerwidget.jsx
@@ -0,0 +1,34 @@
+import NextPrayerWidget from "./NextPrayerWidget";
+import WidgetNav from "./WidgetNav";
+import styles from "./Widget.module.css";
+import { Mosque, Moon, Confetti } from "../../Svg/WidgetLogo";
+const Prayerwidget = () => {
+  return (
+    <div className="prayerWidget-container">
+      <div className={styles["prayerWidget-container-inner"]}>
+        <NextPrayerWidget />
+
+        <WidgetNav
+          title={"UTS Musalla"}
+          logo={<Mosque />}
+          color={"#674A0D"}
+          bg_color={"#DFC693"}
+        />
+        <WidgetNav
+          title={"UTS Jummah"}
+          logo={<Moon />}
+          color={"#35643F"}
+          bg_color={"#A7CFBA"}
+        />
+        <WidgetNav
+          title={"Events"}
+          logo={<Confetti />}
+          color={"#902F46"}
+          bg_color={"#EA9FB1"}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Prayerwidget;
