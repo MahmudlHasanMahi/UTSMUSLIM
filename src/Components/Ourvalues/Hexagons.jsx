c33938d6dac17cf47e5ddfc29017810d9b83746a
Mahmud <jakirmahi20009>
2024-07-09 18:07:09 +0600

    hexgons update

--- src/Components/Ourvalues/Hexagons.jsx
+++ src/Components/Ourvalues/Hexagons.jsx
@@ -50,29 +50,25 @@ const Hexagons = () => {
   };
   const scope = useHexagonAnimation(inView);
   return (
-    <div className="hexagons-main-container">
-      <div ref={scope}>
-        <div className="hexagon-container-1">
-          <Hexagon pkey={1} svg={<Ourvalues_1 />} title={"Quran and Sunnah"} />
-          <Hexagon pkey={2} svg={<Ourvalues_2 />} title={"Strengthen Faith"} />
-        </div>
-        <div className="hexagon-container-2">
-          <Hexagon pkey={6} svg={<Ourvalues_5 />} title={"Remind and Warn"} />
-          <Hexagon pkey={7} svg={<UtsmuslimLogo />} attr={true} />
-          <Hexagon
-            pkey={3}
-            svg={<Ourvalues_3 />}
-            title={"Preserve Teachings"}
-          />
-        </div>
-        <div ref={ref} className="hexagon-container-3">
-          <Hexagon pkey={5} svg={<Ourvalues_6 />} title={"Please Allah ﷻ"} />
-          <Hexagon
-            pkey={4}
-            svg={<Ourvalues_4 />}
-            title={"Foster Relationships"}
-          />
-        </div>
+    <div ref={scope} className="hexagons-main-container">
+      <div className="hexagon-container-1">
+        <Hexagon pkey={1} svg={<Ourvalues_1 />} title={"Quran and Sunnah"} />
+        <Hexagon pkey={2} svg={<Ourvalues_2 />} title={"Strengthen Faith"} />
+      </div>
+
+      <div className="hexagon-container-2">
+        <Hexagon pkey={6} svg={<Ourvalues_5 />} title={"Remind and Warn"} />
+        <Hexagon pkey={7} svg={<UtsmuslimLogo />} attr={true} />
+        <Hexagon pkey={3} svg={<Ourvalues_3 />} title={"Preserve Teachings"} />
+      </div>
+      <div ref={ref} className="hexagon-container-3">
+        <Hexagon pkey={5} svg={<Ourvalues_6 />} title={"Please Allah ﷻ"} />
+        <Hexagon
+          pkey={4}
+          svg={<Ourvalues_4 />}
+          title={"Foster Relationships"}
+        />
+        {/* <span ref={ref}></span> */}
       </div>
     </div>
   );
