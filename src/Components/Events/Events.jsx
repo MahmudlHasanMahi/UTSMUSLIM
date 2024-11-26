de8e3fc1f9ac5e88242bca3e572a2ad3a281d505
Mahmud <jakirmahi20009>
2024-11-26 17:43:59 +0600

    Image check

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -5,34 +5,34 @@ import ImageSlider from "./ImageSlider";
 import image from "./pics/SliderImage1.jpg";
 const Events = () => {
   console.log(image);
-  const Images = [
-    "/src/Components/Events/pics/SliderImage1.jpg",
-    "/src/Components/Events/pics/SliderImage2.jpg",
-    "/src/Components/Events/pics/SliderImage3.jpg",
-    "/src/Components/Events/pics/SliderImage4.png",
-    "/src/Components/Events/pics/SliderImage5.png",
-  ];
+  // const Images = [
+  //   "/src/Components/Events/pics/SliderImage1.jpg",
+  //   "/src/Components/Events/pics/SliderImage2.jpg",
+  //   "/src/Components/Events/pics/SliderImage3.jpg",
+  //   "/src/Components/Events/pics/SliderImage4.png",
+  //   "/src/Components/Events/pics/SliderImage5.png",
+  // ];
   return (
     <div className={styles["event-container"]}>
       <ContentBox />
       <ImageSlider direction={1}>
-        {Images.map((url) => (
-          <div
-            style={{
-              backgroundImage: `url(${url})`,
-            }}
-          />
-        ))}
+        {/* {Images.map((url) => ( */}
+        <div
+          style={{
+            backgroundImage: `url(${image})`,
+          }}
+        />
+        {/* // // ))} */}
       </ImageSlider>
 
       <ImageSlider direction={-1}>
-        {Images.map((url) => (
-          <div
-            style={{
-              backgroundImage: `url(${url})`,
-            }}
-          />
-        ))}
+        {/* {Images.map((url) => ( */}
+        <div
+          style={{
+            backgroundImage: `url(${image})`,
+          }}
+        />
+        {/* ))} */}
       </ImageSlider>
     </div>
   );
