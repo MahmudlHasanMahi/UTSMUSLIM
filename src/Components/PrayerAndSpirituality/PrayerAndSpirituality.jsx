5cec4fc6f9264afa2a0e8a2421f817ef6fd41e09
Mahmud <jakirmahi20009>
2024-11-26 17:29:19 +0600

    Prayer Timing Bug fix

--- src/Components/PrayerAndSpirituality/PrayerAndSpirituality.jsx
+++ src/Components/PrayerAndSpirituality/PrayerAndSpirituality.jsx
@@ -2,23 +2,10 @@ import React from "react";
 import styles from "./PrayerAndSpirituality.module.css";
 import ContentBox from "./ContentBox";
 import Cards from "./Cards";
-import { useEffect, useContext } from "react";
-import NavbarContext from "../../Utils/NavbarContext";
-import { useInView } from "react-intersection-observer";
+
 const PrayerAndSpirituality = () => {
-  const { toogleThemeNavbar } = useContext(NavbarContext);
-  const { ref, inView } = useInView({
-    threshold: 0.4,
-  });
-  useEffect(() => {
-    if (inView) {
-      toogleThemeNavbar("Light");
-    } else {
-      toogleThemeNavbar(null);
-    }
-  }, [inView]);
   return (
-    <div ref={ref} className={styles["PrayerAndSpirituality-container"]}>
+    <div className={styles["PrayerAndSpirituality-container"]}>
       <div className={styles["MainContentBox-container"]}>
         <ContentBox />
       </div>
