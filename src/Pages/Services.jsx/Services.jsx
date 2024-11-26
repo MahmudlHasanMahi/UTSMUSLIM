5b36b85aaca0abe259b3f06d40ad6fb59df56d88
Mahmud <jakirmahi20009>
2024-11-26 17:31:43 +0600

    Navbar Color change

--- src/Pages/Services.jsx/Services.jsx
+++ src/Pages/Services.jsx/Services.jsx
@@ -1,17 +1,23 @@
-import React, { useEffect, lazy, Suspense } from "react";
+import React, { useEffect, lazy, Suspense, useContext } from "react";
 import ServicesLandingPage from "../../Components/ServicesLandingPage/ServicesLandingPage";
 import PrayerAndSpirituality from "../../Components/PrayerAndSpirituality/PrayerAndSpirituality";
 import { setDocumentTitle } from "../../Utils/DocumentTitle";
+import { useInView } from "react-intersection-observer";
 import Events from "../../Components/Events/Events";
+import NavbarContext from "../../Utils/NavbarContext";
 const Services = () => {
+  const { toogleThemeNavbar } = useContext(NavbarContext);
+
+  toogleThemeNavbar("Light");
+
   return (
-    <>
+    <div>
       <Suspense fallback={<div>Loading...</div>}>
         <ServicesLandingPage />
         <PrayerAndSpirituality />
         <Events />
       </Suspense>
-    </>
+    </div>
   );
 };
 
