d1413670343fe5c2c42d5e02bedbdfd347ae3d24
Mahmud <jakirmahi20009>
2024-11-26 17:52:26 +0600

    Fix Images Bug

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -2,37 +2,34 @@ import React from "react";
 import styles from "./Events.module.css";
 import ContentBox from "./ContentBox";
 import ImageSlider from "./ImageSlider";
-import image from "./pics/SliderImage1.jpg";
+import image1 from "./pics/SliderImage1.jpg";
+import image2 from "./pics/SliderImage2.jpg";
+import image3 from "./pics/SliderImage3.jpg";
+import image4 from "./pics/SliderImage4.png";
+import image5 from "./pics/SliderImage5.png";
 const Events = () => {
-  console.log(image);
-  // const Images = [
-  //   "/src/Components/Events/pics/SliderImage1.jpg",
-  //   "/src/Components/Events/pics/SliderImage2.jpg",
-  //   "/src/Components/Events/pics/SliderImage3.jpg",
-  //   "/src/Components/Events/pics/SliderImage4.png",
-  //   "/src/Components/Events/pics/SliderImage5.png",
-  // ];
+  const Images = [image1, image2, image3, image4, image5];
   return (
     <div className={styles["event-container"]}>
       <ContentBox />
       <ImageSlider direction={1}>
-        {/* {Images.map((url) => ( */}
-        <div
-          style={{
-            backgroundImage: `url(${image})`,
-          }}
-        />
-        {/* // // ))} */}
+        {Images.map((image) => (
+          <div
+            style={{
+              backgroundImage: `url(${image})`,
+            }}
+          />
+        ))}
       </ImageSlider>
 
       <ImageSlider direction={-1}>
-        {/* {Images.map((url) => ( */}
-        <div
-          style={{
-            backgroundImage: `url(${image})`,
-          }}
-        />
-        {/* ))} */}
+        {Images.map((image) => (
+          <div
+            style={{
+              backgroundImage: `url(${image})`,
+            }}
+          />
+        ))}
       </ImageSlider>
     </div>
   );
