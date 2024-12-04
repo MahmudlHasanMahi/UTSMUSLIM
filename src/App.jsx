4c7af69724bfc0f0a7da0813d18f6f0507e4f073
Mahmud <jakirmahi20009>
2024-12-04 15:15:13 +0600

    Link update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import Navbar from "./Components/Navigation/Navbar";
 import Footer from "./Components/Footer/Footer";
 import Home from "./Pages/HomePage/Home";
-import Services from "./Pages/Services.jsx/Services";
+import Services from "./Pages/Services/Services";
 import PageRoutes from "./PageRoutes";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { NavbarProvider } from "./Utils/NavbarContext";
 import PageUnavailable from "./Pages/PageUnavailable/PageUnavailable";
+import Members from "./Pages/Member/Members";
+import Store from "./Pages/Store/Store";
+import Contact from "./Pages/Contactus/Contact";
 function App() {
   return (
     <NavbarProvider>
       <Navbar />
       <PageRoutes>
         <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path="/services" exact element={<Services />} />
-          <Route path="/members" exact element={<PageUnavailable />} />
-          <Route path="/blogs" exact element={<PageUnavailable />} />
-          <Route path="/contact-us" exact element={<PageUnavailable />} />
-          <Route path="/apparels" exact element={<PageUnavailable />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/members" element={<Members />} />
+          <Route path="/blogs" element={<PageUnavailable />} />
+          <Route path="/contact-us" element={<Contact />} />
+          <Route path="/contactus" element={<Contact />} />
+          <Route path="/store" element={<Store />} />
         </Routes>
         <Footer />
       </PageRoutes>
